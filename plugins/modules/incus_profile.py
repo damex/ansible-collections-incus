7238945405620da451c19c12618ea058553f36c9
Roman Kuzmitskii <roman@damex.org>
2026-03-04 14:41:24 +0700

    #55: fix sanity test missing-gplv3-license complaint

--- plugins/modules/incus_profile.py
+++ plugins/modules/incus_profile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Copyright: Roman Kuzmitskii <ansible@damex.org>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """Manage Incus profiles."""
