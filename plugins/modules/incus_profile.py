0e0c40b2367fce9d8b663fd063ffaff0a03857d8
Roman Kuzmitskii <roman@damex.org>
2026-03-06 18:33:03 +0700

    NOTASK: adjust order for incus_profile module import

--- plugins/modules/incus_profile.py
+++ plugins/modules/incus_profile.py
@@ -75,10 +75,11 @@ EXAMPLES = r"""
 RETURN = r"""
 """
 
+from typing import Any
+
 from ansible_collections.damex.incus.plugins.module_utils.device import (
     INCUS_DEVICE_OPTIONS,
 )
-from typing import Any
 
 from ansible_collections.damex.incus.plugins.module_utils.incus import (
     INCUS_COMMON_ARGUMENT_SPEC,
